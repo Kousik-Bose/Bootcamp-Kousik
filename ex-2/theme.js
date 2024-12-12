d72a7c55a94321210d154c488a6908b1acecefb6
KOUSIK BOSE <95861235+Kousik-Bose@users.noreply.github.com>
2024-12-13 01:22:50 +0530

    Added my project

---
+++ ex-2/theme.js
@@ -0,0 +1,10 @@
+// theme.js
+document.getElementById("theme-toggle").addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
+    
+    if (document.body.classList.contains("dark-mode")) {
+        this.textContent = "Switch to Light Mode";
+    } else {
+        this.textContent = "Switch to Dark Mode";
+    }
+});
